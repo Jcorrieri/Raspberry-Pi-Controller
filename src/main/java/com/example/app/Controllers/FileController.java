d09b77e4e8b1a51302a0c89bfa1106f156a4ab69
Jacomo <jacomocorrieri@gmail.com>
2023-11-17 14:41:25 -0500

    fixed annoying path bug with "//" at root

--- src/main/java/com/example/app/Controllers/FileController.java
+++ src/main/java/com/example/app/Controllers/FileController.java
@@ -2,35 +2,66 @@ package com.example.app.Controllers;
 
 import com.example.app.App;
 import javafx.fxml.FXML;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import net.schmizz.sshj.sftp.RemoteResourceInfo;
 import net.schmizz.sshj.sftp.StatefulSFTPClient;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 public class FileController {
 
-    TreeItem<String> root = new TreeItem<>("Root");
-
     private StatefulSFTPClient client;
-
-    Image directoryIcon = new Image(Objects.requireNonNull(App.class.getResourceAsStream("Images/dir.png")));
-    Image fileIcon = new Image(Objects.requireNonNull(App.class.getResourceAsStream("Images/file.png")));
 
     @FXML
     private TreeView<String> treeView;
 
+    HashMap<String, FileItem<String>> cachedItems;
+
+    FileItem<String> root;
+
+    Image directoryIcon = new Image(Objects.requireNonNull(App.class.getResourceAsStream("Images/dir.png")));
+    Image fileIcon = new Image(Objects.requireNonNull(App.class.getResourceAsStream("Images/file.png")));
+
+    MenuItem setRoot = new MenuItem("Set Root Directory");
+    MenuItem download = new MenuItem("Download");
+    MenuItem delete = new MenuItem("Delete");
+    MenuItem rename = new MenuItem("Rename");
+
+    ContextMenu contextMenu = new ContextMenu(setRoot, download, delete, rename);
+
     public void init() {
-        treeView.setRoot(root);
+        root = new FileItem<>("Root", null);
+        cachedItems = new HashMap<>();
         client = App.currentPi.createStatefulSTFPClient();
+
+        treeView.setRoot(root);
+        treeView.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.SECONDARY) {
+               FileItem<String> selectedItem = (FileItem<String>) treeView.getSelectionModel().getSelectedItem();
+                if (selectedItem != null) {
+                    contextMenu.show(selectedItem.getGraphic(), event.getScreenX(), event.getScreenY());
+
+                    setRoot.setOnAction(event1 -> setRoot(selectedItem));
+
+                    download.setOnAction(event2 -> System.out.println("Downloading..."));
+
+                    delete.setOnAction(event3 -> remove(selectedItem, selectedItem.getChildren().size() > 0));
+
+                    rename.setOnAction(event4 -> rename(selectedItem, "test232-"));
+                }
+            }
+        });
 
         try {
             root.setValue(client.pwd());
+            root.updatePath(root.getValue());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -45,11 +76,14 @@ public class FileController {
             List<RemoteResourceInfo> list = client.ls();
 
             for (RemoteResourceInfo item : list) {
+                FileItem<String> child = new FileItem<>(item.getName(), item.getPath());
 
-                FileItem<String> child;
+                if (cachedItems.containsKey(child.getPath())) {
+                    rootItem.getChildren().add(cachedItems.get(child.getPath()));
+                    continue;
+                }
 
                 if (item.isDirectory()) {
-                    child = new FileItem<>(item.getName(), client.pwd() + "/" + item.getName());
                     child.getChildren().add(new TreeItem<>("temp"));
 
                     child.expandedProperty().addListener((observable, oldValue, newValue) -> {
@@ -61,7 +95,7 @@ public class FileController {
 
                                 cacheImmediate(child);
 
-                                client.cd(root.getValue());
+                                client.cd(root.getPath());
 
                                 child.setCached(true);
                             } catch (IOException e) {
@@ -72,17 +106,83 @@ public class FileController {
 
                     child.setGraphic(new ImageView(directoryIcon));
                 } else {
-                    child = new FileItem<>(item.getName(), client.pwd() + "/" + item.getName());
                     child.setGraphic(new ImageView(fileIcon));
                 }
 
+                cachedItems.put(child.getPath(), child);
                 rootItem.getChildren().add(child);
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            App.createAlert("Error Opening Directory - " + e.getMessage(), Alert.AlertType.ERROR);
         }
 
         System.out.println("Disk cache time: " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    @FXML
+    private void back() {
+        try {
+            client.cd("..");
+
+            root = new FileItem<>(client.pwd(), client.pwd());
+            treeView.setRoot(root);
+
+            cacheImmediate(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setRoot(FileItem<String> newRoot) {
+        try {
+            client.cd("/");
+            client.cd(newRoot.getPath());
+
+            root = newRoot;
+            treeView.setRoot(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    private void goHome() {
+        try {
+            client.cd("/");
+
+            root = new FileItem<>(client.pwd(), client.pwd());
+            treeView.setRoot(root);
+
+            cacheImmediate(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void remove(FileItem<String> item, boolean directory) {
+        try {
+            if (directory) {
+                client.rmdir(item.getPath());
+            } else {
+                client.rm(item.getPath());
+            }
+
+            cachedItems.remove(item.getPath());
+            item.getParent().getChildren().remove(item);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void rename(FileItem<String> item, String name) {
+        try {
+            String newPath = ((FileItem<String>) item.getParent()).getPath() + "/" + name;
+            client.rename(item.getPath(), newPath);
+            item.updatePath(newPath);
+            item.setValue(name);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static class FileItem<String> extends TreeItem<String> {
